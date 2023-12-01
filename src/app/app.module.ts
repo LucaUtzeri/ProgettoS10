c96efd3d47f31d4ec481ab3440f74875971c5feb
Luca Utzeri <lutzeri@libero.it>
2023-12-01 10:34:39 +0100

    app setup

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,34 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule, Route } from '@angular/router';
 
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { TodoComponent } from './components/todo/todo.component';
+import { CompletedComponent } from './components/completed/completed.component';
+
+const routes: Route[] = [
+  {
+    path: '',
+    component: TodoComponent
+  },
+  {
+    path: 'completed',
+    component: CompletedComponent
+  }
+]
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    TodoComponent,
+    CompletedComponent,
+    TodoComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(routes)
   ],
   providers: [],
   bootstrap: [AppComponent]
